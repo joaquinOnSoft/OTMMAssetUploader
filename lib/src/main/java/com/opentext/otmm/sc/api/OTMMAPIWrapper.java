7e7a398a251e84052d244bc5cadd621df623bf59
JoaquinOnSoft <joaquingp@gmail.com>
2022-03-10 16:40:20 +0100

    New method: Create assets (work in progress)

--- lib/src/main/java/com/opentext/otmm/sc/api/OTMMAPIWrapper.java
+++ lib/src/main/java/com/opentext/otmm/sc/api/OTMMAPIWrapper.java
@@ -1,6 +1,7 @@
 package com.opentext.otmm.sc.api;
 
 import java.io.IOException;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -236,6 +237,35 @@ public class OTMMAPIWrapper {
 		}
 		
 		return assets;
+	}
+	
+	/**
+	 * <strong>Create Asset(s)</strong> with the specified asset representation or import template. 
+	 * This should be a multipart/form-data request. Please use the utf-8 charset 
+	 * encoding so that any unicode characters in names of the attached files are 
+	 * interpreted properly. The operation creates a job and returns the job handle. 
+	 * Using the job id in job handle, you can query the 'jobs' resource for completion status.
+	 * 
+	 * If only an asset representation is specified the user is required to have 
+	 * Apply/Remove Security Policy permission for all security policies assigned. 
+	 * 
+	 * If only an import template is specified the user is not required to have 
+	 * Apply/Remove Security Policy permission for the security policies inherited 
+	 * from the import template. 
+	 * 
+	 * If both an asset representation and an import template are specified the metadata, 
+	 * security policies, and categories populated into the assets will be determined by 
+	 * the asset representation, but the user will only be required to have 
+	 * Apply/Remove Security Policy permission for the security policies not present 
+	 * in the import template.
+	 * 
+	 * <strong>Method</strong>: POST
+	 * <strong>URL</strong>: /v6/assets
+	 * @param sessionId - Session identifier (provided by `sessions` method)
+	 * @see https://developer.opentext.com/apis/14ba85a7-4693-48d3-8c93-9214c663edd2/06c4a79f-3f4a-4a5a-aab9-9519740b27c7/1d6ec9c5-7620-456e-b52f-cfffb2734eb0#operation/createAsset
+	 */
+	public String createAssets(String sessionId, String folderId, Path path) {
+		return null;
 	}
 	
 	/**
